17c5476d243ce2d6efd67ff27e7ced3945b58704
stefano fonzi <stefanofonzi@MacBook-Pro-di-stefano.local>
2026-05-11 08:12:01 +0200

    Aggiunto Meta Pixel Laurea Smart

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import RegisterSW from "./register-sw";
 import OneSignalInit from "./onesignal-init";
+import MetaPixelTracker from "./meta-pixel-tracker";
 
 const sora = Sora({
   subsets: ["latin"],
@@ -37,8 +39,37 @@ export default function RootLayout({
   return (
     <html lang="it">
       <body className={`${sora.variable} antialiased`}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '2224392868096674');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=TUO_PIXEL_ID&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <RegisterSW />
         <OneSignalInit />
+        <MetaPixelTracker />
+
         {children}
       </body>
     </html>
